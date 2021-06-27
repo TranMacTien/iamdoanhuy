9960e29df5f8a4caa2dd95f99fbbffbd8a21a2ec
TienTran <tientranmac96@gmail.com>
2021-06-27 10:45:22 +0700

    update google search html tag

--- src/html.js
+++ src/html.js
@@ -7,6 +7,7 @@ export default function HTML(props) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta name="google-site-verification" content="zN-_5pz0csHT_8MWWw5kAKhbYYNopEWVw5cfh02ZHDs" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
